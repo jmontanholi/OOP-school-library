7c5bcf022f6af79ccfac1cba4b35c81872a06ce7
jmontanholi <jonunes9326@hotmail.com.br>
2021-10-05 18:36:29 -0300

    Error implement

--- Classes/corrector.rb
+++ Classes/corrector.rb
@@ -1,9 +1,13 @@
+require 'pry'
+
 class Corrector
   def correct_name(name)
     if name.length <= 10
-      name.capitalize
-    else
-      name.capitalize[0, 10]
+      result = name.capitalize
+      result.split(//)
+    elsif name.length > 10
+      result = name.capitalize[0, 10]
+      result.split(//)
     end
   end
 end
