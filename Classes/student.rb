2432f13a6968c69eefc3e2eb9ee1842c8852c6a4
jmontanholi <jonunes9326@hotmail.com.br>
2021-10-06 09:53:39 -0300

    Add classroom method to add students and vice-versa

--- Classes/student.rb
+++ Classes/student.rb
@@ -1,9 +1,17 @@
 require_relative 'person'
+require_relative 'classroom'
+require 'pry'
 
 class Student < Person
-  def initialize(classroom, age, name = 'Unkown', parent_permission = true)
+  attr_reader :owner
+
+  def initialize(age, name = 'Unkown', parent_permission = true)
     super(age, name, parent_permission)
+  end
+
+  def classroom=(classroom)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
@@ -11,6 +19,12 @@ class Student < Person
   end
 end
 
-student = Student.new('A1', 15, 'pedro', false)
+student = Student.new(15, 'pedro', false)
 
 student.play_hooky
+
+classroom = Classroom.new
+
+classroom.add_student(student)
+
+binding.pry
