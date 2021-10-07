858b7bb9b81df3d54d077589c24ff5a203584cc4
jmontanholi <jonunes9326@hotmail.com.br>
2021-10-07 14:06:25 -0300

    Fix linter errors

--- main.rb
+++ main.rb
@@ -23,8 +23,8 @@ end
 def create_person(list)
   puts "\n Do you want to create a (0) Student or a (1) Teacher?"
   input = gets.chomp
-  if input == '0' then create_student(list) end
-  if input == '1' then create_teacher(list) end
+  create_student(list) if input == '0'
+  create_teacher(list) if input == '1'
 end
 
 def create_student(list)
@@ -32,13 +32,12 @@ def create_student(list)
   age = gets.chomp
   puts "\nEnter the name of the student"
   name = gets.chomp
-  name == '' ? false : name = name
+  name == '' ? false : name
   puts 'The person has partent_permission?'
   puts '(0) No permission'
   puts '(1) Permission'
   permission = gets.chomp
-  permission == 1 ? true : false
-  student = Student.new(age, name, parent_permission: permission)
+  student = Student.new(age, name, parent_permission: permission == 1)
   add_to_list(list, student)
 end
 
@@ -47,7 +46,7 @@ def create_teacher(list)
   age = gets.chomp
   puts "\nEnter the name of the teacher"
   name = gets.chomp
-  name == '' ? false : name = name
+  name == '' ? false : name
   puts "\nEnter the specialization of the teacher"
   spec = gets.chomp
   teacher = Teacher.new(spec, age, name)
@@ -55,28 +54,28 @@ def create_teacher(list)
 end
 
 def validate_num(input, list)
-  if input.to_i > list.length - 1 || input == ""
-    puts 'Please enter a valid number'
-    input = gets.chomp
-    validate_num(input, list)
-  end
+  return unless input.to_i > (list.length - 1) || input == ''
+
+  puts 'Please enter a valid number'
+  input = gets.chomp
+  validate_num(input, list)
 end
 
 def validate_date(input)
-  if input == ""
-    puts 'Please enter a valid number'
-    input = gets.chomp
-    validate_date(input)
-  end
+  return unless input == ''
+
+  puts 'Please enter a valid number'
+  input = gets.chomp
+  validate_date(input)
 end
 
 def create_rental(list, book_list, people_list)
   puts "\nChoose a book from the list"
-  book_list.each { |b| puts "(#{book_list.index(b)}) Title: '#{b.title}' Author: '#{b.author}'"}
+  book_list.each { |b| puts "(#{book_list.index(b)}) Title: '#{b.title}' Author: '#{b.author}'" }
   book_choice = gets.chomp
   validate_num(book_choice, book_list)
   puts "\nChoose a person"
-  people_list.each { |person| puts "(#{people_list.index(person)}) Name: '#{person.name}' ID: '#{person.id}'"}
+  people_list.each { |person| puts "(#{people_list.index(person)}) Name: '#{person.name}' ID: '#{person.id}'" }
   person_choice = gets.chomp
   validate_num(person_choice, people_list)
   puts "\nPlease enter a day"
@@ -88,27 +87,25 @@ def create_rental(list, book_list, people_list)
   date = "#{day}/#{month}/#{year}"
   validate_date(date)
   rental = Rental.new(date, people_list[person_choice.to_i], book_list[book_choice.to_i])
-  puts "Created successfully"
+  puts 'Created successfully'
   add_to_list(list, rental)
 end
 
 def show_books(list)
-  list.each { |b| puts "\nTitle: '#{b.title}' Author: '#{b.author}'"}
+  list.each { |b| puts "\nTitle: '#{b.title}' Author: '#{b.author}'" }
 end
 
 def show_people(list)
-  list.each { |person| puts "\n[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"}
+  list.each { |person| puts "\n[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
 end
 
 def show_rentals(list)
   puts "\nPlease enter the id of the person"
   id = gets.chomp
-  
+
   puts "\nRentals:"
   list.each do |r|
-    if id.to_i == r.person.id
-      puts "Date: #{r.date}, Book: '#{r.book.title}', Author: '#{r.book.author}'"
-    end
+    puts "Date: #{r.date}, Book: '#{r.book.title}', Author: '#{r.book.author}'" if id.to_i == r.person.id
   end
 end
 
@@ -144,10 +141,10 @@ def input_process(input, book_list, people_list, rentals_list)
   main(book_list, people_list, rentals_list)
 end
 
-def main(book_list = [], people_list = [], rentals_list = [])
-  book_list = book_list
-  people_list = people_list
-  rentals_list = rentals_list
+def main(book_list_input = [], people_list_input = [], rentals_list_input = [])
+  book_list = book_list_input
+  people_list = people_list_input
+  rentals_list = rentals_list_input
   show_options
   user_input = gets.chomp
   input_process(user_input, book_list, people_list, rentals_list)
